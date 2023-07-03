4da1873472eaa391991d4c8ee288fc740d3330fa
SJANETA <smjaneta@uce.edu.ec>
2023-07-03 08:47:05 -0500

    final

--- main.py
+++ main.py
@@ -3,20 +3,19 @@
 import openai
 import uvicorn
 from fastapi import FastAPI, Request
-from uce.ai.Pruebalunes import Document, interface
 from pydantic import BaseModel
 
 
-@app.post('/inference', status_code=200)
-def inference_endpoint(doc: Document):
-    response = inference(doc.prompt)
-    return{
-        'inference': response[0],
-        'usage': response[1]
-    }
+#@app.post('/inference', status_code=200)
+#def inference_endpoint(doc: Document):
+    #response = inference(doc.prompt)
+    #return{
+      #  'inference': response[0],
+     #   'usage': response[1]
+    #}
 
 
-    prompt: str = ''
+    #prompt: str = ''
 
 openai.api_key = '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'
 openai.organization='org-J0y1yI2sOfB5bhErJ2Ul9UrT'
@@ -31,20 +30,7 @@ def read_root():
     return {"Hola": "Mundo"}
 
 
-completion = openai.ChatCompletion.create(
-    model="gtp-3.5-turbo",
-    messages = [
-        {"role": "system","content": """Eres una calculadora factorial, cada numero ingresado, calcula el factorial"""},
-
-        {"role": "user","content": prompt}
-    ]
-)
-
-
-content= completion.choices[0].message.content
-total_tokens= completion.usage.total_tokens
-
-@app.post ("/calculadorafactorial/{numero}")
+@app.get ("/calculadorafactorial/{numero}")
 def calcular_factorial(numero: int):
     if numero < 0:
         return {"error": "El número debe ser un entero no negativo."}
